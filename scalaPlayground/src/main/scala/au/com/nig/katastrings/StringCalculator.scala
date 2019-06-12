4bec185694bb482013dce258a23a234c63d61da2
nicolas <nicolas065@gmail.com>
2019-06-12 15:11:51 +1000

    improvement

--- scalaPlayground/src/main/scala/au/com/nig/katastrings/StringCalculator.scala
+++ scalaPlayground/src/main/scala/au/com/nig/katastrings/StringCalculator.scala
@@ -3,16 +3,19 @@ package au.com.nig.katastrings
 object StringCalculator {
   def add(numbers: String): Int = {
     if (!numbers.isEmpty) {
-      val delimiter = if (numbers.startsWith("//")) numbers.charAt(2) else ','
+      val numbersWithCustomDelimiter = raw"//(.)\n(.*)".r
+      val (delimiter, numberList) = numbers match {
+        case numbersWithCustomDelimiter(d, n) => (d, n)
+        case _ => (",", numbers)
+      }
 
-      val integers =  numbers
-        .stripPrefix(s"//$delimiter\n")
-        .split(delimiter)
-        .flatMap(_.split("\n").map(_.toInt))
+      val integers =  numberList
+        .split(s"[$delimiter\n]")
+        .map(_.toInt)
 
-      val negIntegers = integers.filter(i => i < 0)
+      val negIntegers = integers.filter(_ < 0)
 
-      if (negIntegers.length > 0) throw new Exception(s"negatives: ${negIntegers.mkString(", ")} not allowed")
+      if (negIntegers.nonEmpty) throw new Exception(s"negatives: ${negIntegers.mkString(", ")} not allowed")
       else integers.sum
     } else 0
   }
