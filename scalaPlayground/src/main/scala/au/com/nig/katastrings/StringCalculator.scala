9fb38d08c9f16277904afb522c6ac499db912d82
nicolas <nicolas065@gmail.com>
2019-06-12 10:03:02 +1000

    questions 1 and 2

--- scalaPlayground/src/main/scala/au/com/nig/katastrings/StringCalculator.scala
+++ scalaPlayground/src/main/scala/au/com/nig/katastrings/StringCalculator.scala
@@ -1,5 +1,8 @@
 package au.com.nig.katastrings
 
 object StringCalculator {
-  def add(numbers: String): Int = ???
+  def add(numbers: String): Int = {
+    if (numbers.isEmpty) 0
+    else numbers.split(",").map(_.toInt).sum
+  }
 }
--- scalaPlayground/src/test/scala/au/com/nig/katastrings/StringCalculatorTest.scala
+++ scalaPlayground/src/test/scala/au/com/nig/katastrings/StringCalculatorTest.scala
@@ -15,5 +15,35 @@ class StringCalculatorTest extends WordSpec with Matchers {
       // Then
       result shouldEqual 0
     }
+    "return the number if only one number in the String" in {
+      // Given
+      val testString = "9"
+
+      // When
+      val result = StringCalculator.add(testString)
+
+      // Then
+      result shouldEqual 9
+    }
+    "return the sum of the 2 numbers if there are two numbers in the String" in {
+      // Given
+      val testString = "9,6"
+
+      // When
+      val result = StringCalculator.add(testString)
+
+      // Then
+      result shouldEqual 15
+    }
+    "return the sum of the numbers if there are SEVERAL numbers in the String" in {
+      // Given
+      val testString = "9,6,1,1"
+
+      // When
+      val result = StringCalculator.add(testString)
+
+      // Then
+      result shouldEqual 17
+    }
   }
 }
